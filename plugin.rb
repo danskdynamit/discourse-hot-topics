dad34de3db7ec1b2a1aa0db5c5392e0f9829b61e
Joe Buhlig <joe@joebuhlig.com>
2016-04-27 11:15:00 -0500

    Base plugin files added

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-upvote
+# about: Adds the ability to upvote from the topic list.
+# version: 0.1
+# author: Joe Buhlig joebuhlig.com
+# url: https://github.com/danskdynamit/discourse-upvote
+
+enabled_site_setting :upvote_enabled
